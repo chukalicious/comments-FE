7abfe5983a6c0ae62bc14174eed27d9dfebb168c
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-19 19:49:54 -0500

    created folder structure for the Navbar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { themeChange } from "theme-change";
 import { Route, Routes } from "react-router-dom";
 
+import NavbarContainer from "./components/NavbarComponent";
 import Test from "./components/Test";
 import SignupContainer from "./components/SignupComponent";
 import CommentContainer from "./components/CommentsComponent";
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <div>
+      <NavbarContainer />
       <Test />
       <Routes>
         <Route path="/signup" element={<SignupContainer />} />
