440b73e51073ac790d83f67d168b8b5c9a22b880
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-19 20:08:27 -0500

    created link to signup component

--- src/components/NavbarComponent/Navbar.js
+++ src/components/NavbarComponent/Navbar.js
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return;
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          Comments App
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+
+          <li>
+            <a>Log in</a>
+          </li>
+        </ul>
+      </div>
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src="https://placeimg.com/80/80/people" />
+          </div>
+        </label>
+        <ul
+          tabIndex={0}
+          className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+          <li>
+            <a>Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
