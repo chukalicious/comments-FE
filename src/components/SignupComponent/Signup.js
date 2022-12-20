0289751d3aca4500177e67e5ef44f513ff477666
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-19 19:40:12 -0500

    done with this component for now. will create issues for it

--- src/components/SignupComponent/Signup.js
+++ src/components/SignupComponent/Signup.js
@@ -1,50 +1,86 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-
 import { FaGooglePlusG, FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    setError,
+  } = useForm({
+    criteriaMode: "all",
+  });
 
   const handleFormData = (data) => {
     console.log(data);
     console.log(errors);
   };
+
+  useEffect(() => {
+    setError(
+      "Email",
+      {
+        types: {
+          required: "Please enter your email address",
+          pattern: "^[w-.]+@([w-]+.)+[w-]{2,4}$",
+        },
+      },
+      "Password",
+      { types: { required: true, minLength: 8 } }
+    );
+  }, [setError]);
   return (
-    <div className="flex flex-col w-full mx-auto">
+    <div className="flex flex-col w-full mx-auto mt-10">
+      <div className="text-center">
+        <h1 className="font-bold text-2xl py-2">Create an account.</h1>
+        <h2>
+          Already a member?{" "}
+          <span className="text-info hover:underline">
+            <Link to="">Sign In</Link>
+          </span>
+        </h2>
+      </div>
       <form
         onSubmit={handleSubmit(handleFormData)}
         className="flex w-[95%] mx-auto"
       >
         <div className="flex flex-col w-full py-7 px-[.35rem] ">
           <label className="label">
-            <span className="label-text">You email</span>
+            <span className="label-text">
+              Your email<span className="text-error"> *</span>
+            </span>
           </label>
           <input
             type="email"
-            {...register(
-              "Email",
-              { required: true, max: 120 },
-              {
-                pattern:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-              }
-            )}
-            aria-invalid={errors.firstName ? "true" : "false"}
+            {...register("Email")}
             placeholder="Enter your email"
             className="input input-bordered input-primary max-w-sm"
           />
+          {errors.Email && errors.Email.types && (
+            <p className="pt-2 ml-2 text-error text-sm">
+              {errors.Email.types.required}
+            </p>
+          )}
           <label className="label">
             <span className="label-text">Your password</span>
           </label>
           <input
-            {...register("Password", { required: true, max: 119, min: 5 })}
+            {...register("Password")}
             placeholder="Enter your password"
             className="input input-bordered input-primary max-w-sm"
           />
+          {errors.Password && (
+            <p className="pt-2 ml-2 text-error text-sm">
+              {errors.Password.types.required}
+            </p>
+          )}
+          {errors.Password && errors.Password.types && (
+            <p className="pt-2 ml-2 text-error text-sm">
+              {errors.Password.types.required}
+            </p>
+          )}
           <label className="label">
             <span className="label-text">Re-enter Password</span>
           </label>
@@ -53,6 +89,11 @@ const Signup = () => {
             placeholder="Re-enter your password"
             className="input input-bordered input-primary max-w-sm"
           />
+          {errors.Repassword && (
+            <p className="pt-2 ml-2 text-error text-sm">
+              Pease re-enter your password
+            </p>
+          )}
           <label className="label">
             <span className="label-text-alt text-error">Forgot Password?</span>
           </label>
