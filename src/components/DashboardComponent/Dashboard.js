2f72b4352ce8887823220fdd383d3ae81ffb4251
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-29 02:07:22 -0500

    imported the comment container to the dashboard component

--- src/components/DashboardComponent/Dashboard.js
+++ src/components/DashboardComponent/Dashboard.js
@@ -1,5 +1,15 @@
+import CommentContainer from "../CommentsComponent";
 const Dashboard = () => {
-  return <div>This is the Dashboard</div>;
+  return (
+    <div>
+      <div>
+        <p>The actual dashboard will go in this above section</p>
+      </div>
+      <div>
+        <CommentContainer />{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
