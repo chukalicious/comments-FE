b071a9d50845a2fe8e9215602ce9f20253abf724
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-29 19:05:41 -0500

    changed navbar color

--- src/components/NavbarComponent/Navbar.js
+++ src/components/NavbarComponent/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-neutral text-neutral-content">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost normal-case text-xl">
           Commentor.io
