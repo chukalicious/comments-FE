086a7b09fd088728d3174c28cda3dd3e02c2f6db
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-30 04:48:44 -0500

    changed theme to winter

--- src/components/VoteComponent/VoteButtons.js
+++ src/components/VoteComponent/VoteButtons.js
@@ -2,9 +2,9 @@ const VoteButtons = () => {
   return (
     <div className="form-control mx-2 pb-3">
       <button className="input-group bg-base-300 rounded-2xl ">
-        <span className="text-[#AF60FF]">+</span>
-        <p className="text-xl p-2 w-16 font-semibold text-[#853BFF]">10</p>
-        <span className="text-[#AF60FF]">-</span>
+        <span className="text-primary">+</span>
+        <p className="text-xl p-2 w-16 font-semibold text-primary-focus">10</p>
+        <span className="text-primary">-</span>
       </button>
     </div>
   );
